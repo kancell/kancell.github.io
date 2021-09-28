87e31faa721abba6cd0db129fcba65e854841070
kancell <2661030790@qq.com>
2021-09-28 18:05:03 +0800

    update

--- client/p__error__304.js
+++ client/p__error__304.js
@@ -1,1 +1,1 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[19],{CZ3A:function(n,c,e){"use strict";e.r(c);e("q1tI");var i=e("nKUr");c["default"]=()=>Object(i["jsx"])("div",{children:"304"})}}]);
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[23],{CZ3A:function(n,c,e){"use strict";e.r(c);e("q1tI");var i=e("nKUr");c["default"]=()=>Object(i["jsx"])("div",{children:"304"})}}]);
