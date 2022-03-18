e68cc34711a8c9db1e3355421d2314da8266c27e
kancell <2661030790@qq.com>
2022-03-18 06:44:27 +0000

    Deploying to main from @ kancell/wuhan-WeChat-app@1cfcfd2acb82b36da59516704f4e0f925dd84c01 🚀

--- client/p__supplier__detail.72f30bfe.async.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[95],{Exvl:function(n,c,e){"use strict";e.r(c);var t=e("nKUr"),i="Lh6M",a=()=>Object(t["jsx"])("div",{_nk:"".concat(i,"11"),children:Object(t["jsx"])("h1",{_nk:"".concat(i,"21"),children:"Detail"})});c["default"]=a}}]);
